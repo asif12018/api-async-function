52357a9f3e80c4e9d3631fb78629974215bad084
asif12018 <asif.request@gmail.com>
2024-02-26 21:46:50 +0600

    fucntion updated

--- fetch.js
+++ fetch.js
@@ -7,4 +7,14 @@ const loadComments = async() =>{
     const data = await res.json();
     console.log(data);
 
+    //if u want to catch error
+    // try{
+    //     const res =  await fetch('https://jsonplaceholder.typicode.com/comments');
+    //     const data = await res.json();
+    //     console.log(data);
+    // }
+    // catch(error){
+    //     console.log('data load error')
+    // }
+
 }
