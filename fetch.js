ea2282e04d93e72b9bed68cd676514446e5d3435
asif12018 <asif.request@gmail.com>
2024-02-26 21:42:43 +0600

    first commit

---
+++ fetch.js
@@ -0,0 +1,10 @@
+
+
+
+
+const loadComments = async() =>{
+    const res =  await fetch('https://jsonplaceholder.typicode.com/comments');
+    const data = await res.json();
+    console.log(data);
+
+}
